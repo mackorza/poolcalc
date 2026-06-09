8508fb3360d12f22ba53d523fba1a08396204ee8
Gerhard McDonald <gerhard@mackor.co.za>
2026-06-09 19:15:10 +0200

    Release v0.2606.01
    Deploy to futronix.co.za

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/poolcalc',
+  poweredByHeader: false,
   env: {
     NEXT_PUBLIC_BASE_PATH: '/poolcalc',
   },
